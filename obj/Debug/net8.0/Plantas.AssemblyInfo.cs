3e8e41c384b8f3695b755803e7299cf6dbc8c60e
Gustavo Bernal <gustavober98@gmail.com>
2024-10-31 15:39:33 -0500

    feat: update

--- obj/Debug/net8.0/Plantas.AssemblyInfo.cs
+++ obj/Debug/net8.0/Plantas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plantas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dffc660772040b1cab2bc87690cf072b5ebcfdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60126b9a0f06c1444d63a616786adf299891fdb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plantas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plantas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
